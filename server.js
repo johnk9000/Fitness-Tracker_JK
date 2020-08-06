1ba48cf2b2a69fcbe24264fd70c8d4b353a3cc4f
John Kim <johnk9000@gmail.com>
2020-08-06 17:02:33 -0500

    GET routes for exercise and workouts

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
 // HTML Routes ===================================================================================
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "./public/index.html"));
@@ -23,8 +24,39 @@ app.get("/exercise", (req, res) => {
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "./public/stats.html"));
   });
-// API Routes ====================================================================================
 
+// API Routes ====================================================================================
+var db = require("./models")
+
+app.get("/api/workouts", (req, res) => {
+        console.log("/api/workouts" + req) // DEL
+    db.Workout.find({})
+    .populate("exercises")
+    .then(dbWorkout => { res.json(dbWorkout) })
+})
+
+app.get("/api/workouts/:id", (req, res) => {
+        console.log("/api/workouts: " + id + " | " + req) // DEL
+    db.Workout.find({ _id: req.params.id })
+    .then( dbWorkout => { res.json(dbWorkout) })
+    .catch( err => { res.json(err) })
+})
+
+app.get("/api/exericse/", (req, res) => {
+        console.log("/api/workouts" + req)  // DEL
+    db.Exercise.find({})
+    .then( dbExercise => { res.json(dbExercise) })
+    .catch( err => { res.json(err) })
+});
+
+app.get("/api/exericse/:id", (req, res) => {
+        console.log("/api/exercises: " + id + " | " + req)  // DEL
+    db.Exercise.find({ _id: req.params.id })
+    .then(dbExercise => { res.json(dbExercise) })
+    .catch(err => { res.json(err) })
+});
+
+// App Init ==========================================================
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
   });
