d216198906e77ae0c33b77c31e2d9f130156f593
John Kim <johnk9000@gmail.com>
2020-08-06 21:21:48 -0500

    A little confused, but here goes nuttin'

--- public/js/api.js
+++ public/js/api.js
@@ -12,7 +12,7 @@ const API = {
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
-
+      console.log("exercise id: " + id, "(location.search.split("=")[1];)") // DEL
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
--- server.js
+++ server.js
@@ -1,57 +1,64 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
+const express = require("express")
+const path = require("path")
+const mongoose = require("mongoose")
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+const app = express()
+const PORT = process.env.PORT || 8080
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+var db = require("./models")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// CXN to Mongoose =========
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
 
 // HTML Routes ===================================================================================
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"))
   });
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
   });
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
   });
 
 // API Routes ====================================================================================
-var db = require("./models")
 
 app.get("/api/workouts", (req, res) => {
         console.log("/api/workouts" + req) // DEL
     db.Workout.find({})
-    .populate("exercises")
+    //.populate("exercises")
     .then(dbWorkout => { res.json(dbWorkout) })
 })
 
-app.get("/api/workouts/:id", (req, res) => {
-        console.log("/api/workouts: " + id + " | " + req) // DEL
-    db.Workout.find({ _id: req.params.id })
-    .then( dbWorkout => { res.json(dbWorkout) })
-    .catch( err => { res.json(err) })
-})
+// app.get("/api/workouts/:id", (req, res) => {
+//         console.log("/api/workouts: " + id + " | " + req) // DEL
+//     db.Workout.find({ _id: objectId(req.params.id) })
+//     .then( dbWorkout => { res.json(dbWorkout) })
+//     .catch( err => { res.json(err) })
+// })
 
-app.get("/api/exericse/", (req, res) => {
+app.get("/api/exercise", (req, res) => {
         console.log("/api/workouts" + req)  // DEL
     db.Exercise.find({})
     .then( dbExercise => { res.json(dbExercise) })
     .catch( err => { res.json(err) })
 });
+// Add an exercise regiment to the Workouts Schema
+app.post("/api/workouts", (req, res) => {
+  console.log("POST /api/workouts" + req)  // DEL
+    db.Exercise.create(req.body)
+    .then(({ _id }) => { db.Workout.findOneAndUpdate({}, {$push: {exercise: _id}}, {new: true}) })
+    .then( dbExercise => { res.json(dbExercise) })
+    .catch( err => { res.json(err) })
+})
 
-app.get("/api/exericse/:id", (req, res) => {
+app.get("/api/exercise/:id", (req, res) => {
         console.log("/api/exercises: " + id + " | " + req)  // DEL
-    db.Exercise.find({ _id: req.params.id })
+    db.Exercise.find({ _id: objectId(req.params.id) })
     .then(dbExercise => { res.json(dbExercise) })
     .catch(err => { res.json(err) })
 });
